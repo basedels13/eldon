6df1d33e680b1cdeecfc03fcb3d2e2aa12718911
basedels13 <basedels13@gmail.com>
2025-01-31 15:26:13 +0900

    v1.00 season2 deploy

--- public/don/donsword0.77.js
+++ public/don/donsword0.77.js
@@ -8,7 +8,7 @@ window.onload = function(){
   
   function draw(){
   var titletext="v1.00/Click to START";
-  var debugmode=true;  //コンソールログの表示の切り替え/テストプレイ用　リリース時にfalseに
+  var debugmode=false;  //コンソールログの表示の切り替え/テストプレイ用　リリース時にfalseに
   //自分自身の情報を入れる箱
   var IAM = {
     token: null,    // 戸別管理用のトークン
