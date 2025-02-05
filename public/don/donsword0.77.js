a1b621e5ec20b33893a4a971c32b11556d993039
basedels13 <basedels13@gmail.com>
2025-02-05 22:04:03 +0900

    v1.003 betatest

--- public/don/donsword0.77.js
+++ public/don/donsword0.77.js
@@ -6,7 +6,7 @@ window.onload = function(){
   };
   
   function draw(){
-  var titletext="v1.002/Click to START";
+  var titletext="v1.003/Click to START";
   var debugmode=false;  //コンソールログの表示の切り替え/テストプレイ用　リリース時にfalseに
   //自分自身の情報を入れる箱
   var IAM = {
